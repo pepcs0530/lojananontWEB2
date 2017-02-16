3a6a6bf1945bd5053d37f3b9032e64df3d30feb2
pepcs0530 <apiwich_cs_kmitl@hotmail.com>
2017-02-16 22:38:47 +0700

    ใส่โลโก้

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ Licence URI: http://www.os-templates.com/template-terms
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+        
+        <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+        <link rel="icon" href="images/logo.png" type="image/x-icon">
     </head>
     <body id="top">
         <!-- ################################################################################################ -->
